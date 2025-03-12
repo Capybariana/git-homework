924a6463eaddccbc290ad12c0a86efa8d01860ae
Capybariana <novikova.polina.p@gmail.com>
2025-03-12 22:34:06 +0700

    add 2.5 tests in tests.cpp

--- test-repo/tests.cpp
+++ test-repo/tests.cpp
@@ -66,6 +66,21 @@ TEST(CandleTest, BodySize_ZeroBody) {
     Candle c(100, 110, 90, 100);
     EXPECT_DOUBLE_EQ(c.body_size(), 0);
 }
+// 2.5 is_red
+TEST(CandleTest, IsRed_TrueCase) {
+    Candle c(105, 110, 90, 100);
+    EXPECT_TRUE(c.is_red());
+}
+
+TEST(CandleTest, IsRed_FalseCase) {
+    Candle c(100, 110, 90, 105);
+    EXPECT_FALSE(c.is_red());
+}
+
+TEST(CandleTest, IsRed_EqualOpenClose) {
+    Candle c(100, 110, 90, 100);
+    EXPECT_FALSE(c.is_red());
+}
 
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
