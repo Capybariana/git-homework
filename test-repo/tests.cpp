d9073de99fd8edfef9c9b2fcf30698cb6b9de945
Polina Novikova <novikova.polina.p@gmail.com>
2025-05-21 17:00:46 +0700

    Update 3rd lab and new test

--- test-repo/tests.cpp
+++ test-repo/tests.cpp
@@ -97,6 +97,10 @@ TEST(CandleTest, IsGreen_EqualOpenClose) {
     EXPECT_FALSE(c.is_green());
 }
 
+TEST(SimpleMathTest, OneplusOneEqualsTwo){
+    EXPECT_EQ(1 + 1, 2);
+}
+
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
