b95aa8c9dd8a325b0a10ca6103a2328c802586bf
Polina Novikova <novikova.polina.p@gmail.com>
2025-05-22 11:15:23 +0700

    New test

--- tests.cpp
+++ tests.cpp
@@ -101,6 +101,10 @@ TEST(SimpleMathTest, OneplusOneEqualsTwo){
     EXPECT_EQ(1 + 1, 2);
 }
 
+TEST(SimpleMathTest, OneplusTwoEqualsThree){
+    EXPECT_EQ(1 + 2, 3);
+}
+
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
